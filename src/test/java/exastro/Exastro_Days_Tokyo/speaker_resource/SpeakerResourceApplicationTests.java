3f22b9e9487e318e242e76e52998efceee4cd35e
yoshiyuki-konuma <yoshiyuki-konuma@ncontr.com>
2021-12-23 14:54:09 +0900

    test skip.

--- src/test/java/exastro/Exastro_Days_Tokyo/speaker_resource/SpeakerResourceApplicationTests.java
+++ src/test/java/exastro/Exastro_Days_Tokyo/speaker_resource/SpeakerResourceApplicationTests.java
@@ -18,7 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EventUserApplicationTests {
+class SpeakerResourceApplicationTests {
 
 	@Test
 	void contextLoads() {
